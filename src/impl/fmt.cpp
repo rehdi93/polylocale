3e178073741def52caec6a6e51f04fc629234a03
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2020-08-14 18:00:11 -0300

    add regex parsing

--- src/impl/fmt.cpp
+++ src/impl/fmt.cpp
@@ -4,28 +4,48 @@
 #include <iterator>
 #include <functional>
 #include <locale>
+#include <regex>
 
 
 namespace
 {
 
-constexpr char FMT_START   = '%';
-constexpr char FMT_FLAGS[] = "-+#0 ";
-constexpr char FMT_SIZES[] = "hljztI";
-constexpr char FMT_TYPES[] = "CcudioXxEeFfGgAapSsn";
-//constexpr char FMT_ALL[] = "%.*" "-+ #0" "hljztI" "CcudioXxEeFfGgAapSsn" /*"123456789"*/;
+#define CH_FLAGS "-+#0 "
+#define CH_TYPES "CcudioXxEeFfGgAapSsn"
+
+constexpr char FMT_START = '%';
+constexpr char FMT_LENGTHMOD[] = "hljztLI";
+#define RX_LENGTHMOD "h|hh|l|ll|j|z|t|L|I32|I64"
+constexpr char FMT_FLAGS[] = CH_FLAGS;
+constexpr char FMT_TYPES[] = CH_TYPES;
 
 // precision
 constexpr char FMT_PRECISION = '.';
 // value from VA
 constexpr char FMT_FROM_VA = '*';
 
+// format specifier regex pattern
+// https://regex101.com/r/Imw6fT/1
+constexpr auto FMT_PATTERN = "%"
+    "([" CH_FLAGS "]+)?"
+    "([0-9]+|\\*)?"
+    "(\\.[0-9]+|\\*)?"
+    "(" RX_LENGTHMOD ")?"
+    "([" CH_TYPES "])";
+
+
+#undef CH_FLAGS
+#undef CH_TYPES
+#undef RX_LENGTHMOD
+
 } // unnamed namespace
 
 using std::begin; using std::end;
 using std::find;
 
-bool red::polyloc::isfmtflag(char ch, bool zero, bool space)
+namespace red::polyloc {
+
+bool isfmtflag(char ch, bool zero, bool space)
 {
     auto end_ = end(FMT_FLAGS);
     auto it = find(begin(FMT_FLAGS), end_, ch);
@@ -42,19 +62,19 @@ bool red::polyloc::isfmtflag(char ch, bool zero, bool space)
     else return false;
 }
 
-bool red::polyloc::isfmttype(char ch)
+bool isfmttype(char ch)
 {
     auto end_ = end(FMT_TYPES);
     return find(begin(FMT_TYPES), end_, ch) != end_;
 }
 
-bool red::polyloc::isfmtsize(char ch)
+bool isfmtsize(char ch)
 {
-    auto end_ = end(FMT_SIZES);
-    return find(begin(FMT_SIZES), end_, ch) != end_;
+    auto end_ = end(FMT_LENGTHMOD);
+    return find(begin(FMT_LENGTHMOD), end_, ch) != end_;
 }
 
-bool red::polyloc::isfmtchar(char ch, bool digits)
+bool isfmtchar(char ch, bool digits)
 {
     return ch == FMT_PRECISION || ch == FMT_FROM_VA ||
         isfmtflag(ch, digits) || isfmtsize(ch) || isfmttype(ch)
@@ -62,14 +82,10 @@ bool red::polyloc::isfmtchar(char ch, bool digits)
 }
 
 
-namespace red::polyloc {
-
-const int fmtspec_t::VAL_VA = -(int)FMT_FROM_VA;
-
 bool fmt_separator::operator() (iterator& next, iterator end, std::string& token) {
-    using std::adjacent_find;
-
     auto start = next;
+    bool in_fmt = false;
+
     if (start == end)
         return false;
     
@@ -86,16 +102,16 @@ bool fmt_separator::operator() (iterator& next, iterator end, std::string& token
         }
         else {
             // possible printf fmt
-            m_infmt = true;
+            in_fmt = true;
         }
     }
 
-    if (m_infmt)
+    if (in_fmt)
     {
         // "%# +o| %#o" "%10.5d|:%10.5d"
         ptrdiff_t offs = 1;
         next++;
-        auto fmtend = adjacent_find(next, end, [&](char l, char r) mutable {
+        auto fmtend = std::adjacent_find(next, end, [&](char l, char r) mutable {
             if (isfmttype(l)) {
                 return true;
             }
@@ -110,7 +126,6 @@ bool fmt_separator::operator() (iterator& next, iterator end, std::string& token
         next = fmtend + offs;
 
         token.assign(start, next);
-        m_infmt = false;
     }
     else
     {
@@ -119,11 +134,6 @@ bool fmt_separator::operator() (iterator& next, iterator end, std::string& token
     }
 
     return true;
-}
-
-void fmt_separator::reset()
-{
-    m_infmt = false;
 }
 
 
@@ -136,7 +146,7 @@ fmtspec_t parsefmt(red::string_view spec, std::locale const& locale)
     size_t pos = 1;
 
     // Flags
-    auto i = spec.find_first_not_of(FMT_FLAGS, 1);
+    auto i = spec.find_first_not_of(FMT_FLAGS, pos);
     if (i != npos)
     {
         fmtspec.flags = spec.substr(1, i-1);
@@ -165,10 +175,10 @@ fmtspec_t parsefmt(red::string_view spec, std::locale const& locale)
         pos++;
         if (spec[pos] == FMT_FROM_VA)
         {
-            fmtspec.precision = -(int)FMT_FROM_VA;
+            fmtspec.precision = fmtspec.VAL_VA;
             pos++;
         }
-        else if (isdigit(spec[pos], locale)) try
+        else try
         {
             size_t converted;
             fmtspec.precision = svtol(spec.substr(pos), &converted);
@@ -180,7 +190,7 @@ fmtspec_t parsefmt(red::string_view spec, std::locale const& locale)
     }
     
     // size overrides
-    i = spec.find_first_of(FMT_SIZES, pos);
+    i = spec.find_first_of(FMT_LENGTHMOD, pos);
     if (i != npos)
     {
         pos = i;
@@ -188,7 +198,7 @@ fmtspec_t parsefmt(red::string_view spec, std::locale const& locale)
             i = spec.find_first_not_of("6432", pos);
         }
         else {
-            i = spec.find_first_not_of(FMT_SIZES, pos);
+            i = spec.find_first_not_of(FMT_LENGTHMOD, pos);
         }
         
         fmtspec.length_mod = spec.substr(pos, i - pos);
@@ -199,6 +209,70 @@ fmtspec_t parsefmt(red::string_view spec, std::locale const& locale)
     i = spec.find_first_of(FMT_TYPES, pos);
     if (i != npos)
         fmtspec.conversion = spec.at(i);
+
+    return fmtspec;
+}
+
+
+fmtspec_t parsefmt2(std::string const& spec)
+{
+    using red::string_view;
+
+    const std::regex rx{ FMT_PATTERN };
+    enum match_groups : size_t {
+        FullMatch,
+        Flags, FieldWidth, Precision, LengthMod, Type
+    };
+    fmtspec_t fmtspec;
+
+    std::smatch matches;
+    if (std::regex_match(spec, matches, rx))
+    {
+        fmtspec.fmt = spec;
+        fmtspec.conversion = *matches[Type].first;
+        //const auto start = std::addressof(*matches[FullMatch].first);
+
+        const auto start = spec.data();
+
+        if (matches[Flags].matched) {
+            auto ptr = start + matches.position(Flags);
+            fmtspec.flags = string_view(ptr, matches.length(Flags));
+        }
+        if (matches[LengthMod].matched) {
+            auto ptr = start + matches.position(LengthMod);
+            fmtspec.length_mod = string_view(ptr, matches.length(LengthMod));
+        }
+
+        if (matches[FieldWidth].matched) {
+            auto ptr = start + matches.position(FieldWidth);
+
+            if (*ptr == FMT_FROM_VA) {
+                fmtspec.field_width = fmtspec.VAL_VA;
+            }
+            else try {
+                auto num = string_view(ptr, matches.length(FieldWidth));
+                fmtspec.field_width = svtol(num);
+            }
+            catch (const std::exception&) {
+                // no conversion took place
+            }
+        }
+        if (matches[Precision].matched) {
+            auto ptr = start + matches.position(Precision) + 1 /* skip '.' */;
+
+            if (*ptr == FMT_FROM_VA) {
+                fmtspec.precision = fmtspec.VAL_VA;
+            }
+            else try {
+                auto num = string_view(ptr, matches.length(Precision));
+                fmtspec.precision = svtol(num);
+            }
+            catch (const std::exception&) {
+                // no conversion took place
+            }
+        }
+
+    }
 
     return fmtspec;
 }
--- src/impl/printf_fmt.hpp
+++ src/impl/printf_fmt.hpp
@@ -15,10 +15,9 @@ namespace red::polyloc
         using iterator = std::string::const_iterator;
 
         bool operator() (iterator& next, iterator end, std::string& token);
-        void reset();
+        void reset() {}
 
     private:
-        bool m_infmt = false;
     };
 
     using fmt_tokenizer = boost::tokenizer<fmt_separator, fmt_separator::iterator>;
@@ -26,13 +25,17 @@ namespace red::polyloc
     // %[flags][width][.precision][size]type
     struct fmtspec_t
     {
+        static constexpr int VAL_VA = -(int)'*', VAL_AUTO = -1;
+        static constexpr char ILL_FORMED = '\030';
+
+        // the full format specefier, maybe change to std::string?
         red::string_view fmt;
+
         red::string_view flags, length_mod;
         int field_width = -1, precision = -1;
-        char conversion = '\030';
-
-        static const int VAL_VA, VAL_AUTO = -1;
+        char conversion = ILL_FORMED;
     };
 
     fmtspec_t parsefmt(string_view fmt, std::locale const& locale);
+    fmtspec_t parsefmt2(std::string const& spec);
 }
